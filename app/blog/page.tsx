1fa91220b75637a46129aa40ca9e3b713f489f90
jamesdhw <jameshaworthwheatman@gmail.com>
2022-11-21 23:51:50 +0000

    remove views

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -31,9 +31,9 @@ export default async function Blog() {
                 <h4 className="w-full mb-2 text-lg font-medium text-gray-900 md:text-xl dark:text-gray-100">
                   {title}
                 </h4>
-                <p className="w-32 mb-4 text-left text-gray-500 md:text-right md:mb-0">
+                {/* <p className="w-32 mb-4 text-left text-gray-500 md:text-right md:mb-0">
                   {`${new Number(100).toLocaleString()} views`}
-                </p>
+                </p> */}
               </div>
               <p className="text-gray-600 dark:text-gray-400">{summary}</p>
             </div>
