4c83b41c742261ea6d828998c47e47a36af58af1
jamesdhw <jameshaworthwheatman@gmail.com>
2022-11-30 00:24:52 +0000

    grid reverse

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,15 +7,7 @@ export default function About() {
         Hey! I&apos;m James
         <span className="inline-block ml-2 animate-waving-hand">👋</span>
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 w-100 justify-center flex-wrap">
-        <div className="relative w-100 sm:w-72 h-96 overflow-hidden rounded-lg m-5 hover:scale-[1.05] transition-all hover:shadow-xl">
-          <Image
-            className="object-cover"
-            src="/windemere.jpeg"
-            alt="Me crossing a river"
-            fill
-          />
-        </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 w-100 ltr justify-center flex-wrap">
         <div className="flex flex-col justify-evenly align-middle text-xl">
           <p className="py-2">I&apos;m a software engineer living in London!</p>
           <p className="py-2">
@@ -23,13 +15,22 @@ export default function About() {
             <span className="font-semibold">Architect Developer</span> on
             full-stack web and mobile applications at{" "}
             <a href="https://theodo.co.uk" className="font-semibold underline">
-              Theodo
+              Theodo UK
             </a>
             .
           </p>
           <p className="py-2">
-            Fun fact: everyone in my family has a black-belt in Taekwondo
+            <span className="font-semibold">Fun Fact:</span> everyone in my
+            family has a black-belt in Taekwondo.
           </p>
+        </div>
+        <div className="relative sm:order-none order-first w-100 sm:w-72 h-96 overflow-hidden rounded-lg m-5 hover:scale-[1.02] transition-all hover:shadow-xl">
+          <Image
+            className="object-cover"
+            src="/windemere.jpeg"
+            alt="Me stood in front of Windemere in the Lake District"
+            fill
+          />
         </div>
       </div>
     </div>
