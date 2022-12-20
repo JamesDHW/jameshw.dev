13006c965af0d12413efe8f67d1c877394d5358d
jamesdhw <jameshaworthwheatman@gmail.com>
2022-12-20 20:02:06 +0000

    prevent pointer events on before and after

--- components/Home/BookShelf/BookShelf.tsx
+++ components/Home/BookShelf/BookShelf.tsx
@@ -10,8 +10,8 @@ export const BookShelf: FC<BookShelfProps> = ({ children }) => {
     <div
       className={classes(
         "relative flex border-b-2 overflow-x-hidden pb-4",
-        "before:bg-gradient-to-r before:z-10 before:absolute before:w-24 before:top-0 before:bottom-0",
-        "after:bg-gradient-to-l after:z-10 after:absolute after:w-24 after:top-0 after:bottom-0 after:right-0",
+        "before:bg-gradient-to-r before:z-10 before:absolute before:w-24 before:top-0 before:bottom-0 before:pointer-events-none",
+        "after:bg-gradient-to-l after:z-10 after:absolute after:w-24 after:top-0 after:bottom-0 after:right-0 after:pointer-events-none",
         "dark:from-gray-900 from-white to-transparent"
       )}
     >
