a83199e2330284e52fc7a8ca21405a80acf23cb3
jamesdhw <jameshaworthwheatman@gmail.com>
2023-04-16 16:25:16 +0100

    fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,13 @@ const RootLayout: FC<RootPageProps> = ({ children }) => {
 
 export const metadata: Metadata = {
   title: "James Haworth Wheatman",
+  icons: [
+    {
+      rel: "icon",
+      url: "",
+      href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%22100%22>🌊</text></svg>",
+    },
+  ],
 };
 
 export default RootLayout;
