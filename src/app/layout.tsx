8dbf9efd47fcca2025d0349692a950e7c6a3bf77
jamesdhw <jameshaworthwheatman@gmail.com>
2023-06-04 18:32:06 +0100

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
   icons: [
     {
       rel: "icon",
-      url: "",
+      url: "/favicon.svg",
       href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%22100%22>🌊</text></svg>",
     },
   ],
