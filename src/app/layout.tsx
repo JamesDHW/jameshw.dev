bc00d2d4c52e9d9778bdc9826c71800c39e3acf8
jamesdhw <jameshaworthwheatman@gmail.com>
2023-08-06 15:55:40 +0100

    new code styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,11 @@ import { ThemeProvider } from "styles/ThemeProvider";
 import { Navbar } from "components/Layout/Navbar/Navbar";
 import { Footer } from "components/Layout/Footer/Footer";
 import "styles/globals.css";
+import "styles/code.css";
 
 // Notion CSS
 import "react-notion-x/src/styles.css";
 import "katex/dist/katex.min.css";
-import "prismjs/themes/prism-tomorrow.css";
 
 type RootPageProps = { children: ReactNode };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -57,6 +57,10 @@ module.exports = {
         marquee: "marquee 25s linear infinite",
         marquee2: "marquee2 25s linear infinite",
       },
+      boxShadow: {
+        "3xl":
+          "rgba(0, 0, 0, 0.25) 0px 20px 20px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -1px 2px;",
+      },
     },
   },
   plugins: [],
