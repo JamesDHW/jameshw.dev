85b1077818dc3f7d3a851c581e1832f01e09d987
jamesdhw <jameshaworthwheatman@gmail.com>
2024-01-28 23:36:53 +0000

    fix: invalid svg prop

--- src/icons/video.tsx
+++ src/icons/video.tsx
@@ -12,7 +12,7 @@ export const VideoIcon = (props: JSX.IntrinsicElements["svg"]) => {
         <path
           d="M 45 0 C 20.187 0 0 20.187 0 45 c 0 24.813 20.187 45 45 45 c 24.813 0 45 -20.187 45 -45 C 90 20.187 69.813 0 45 0 z M 61.806 47.026 l -12.91 7.453 l -12.91 7.453 c -1.559 0.9 -3.508 -0.225 -3.508 -2.026 V 45 V 30.093 c 0 -1.801 1.949 -2.926 3.508 -2.026 l 12.91 7.453 l 12.91 7.453 C 63.366 43.875 63.366 46.125 61.806 47.026 z"
           transform=" matrix(1 0 0 1 0 0) "
-          stroke-linecap="round"
+          strokeLinecap="round"
           fill="currentColor"
         />
       </g>
