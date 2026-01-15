187929fbc13049332b956284cf80c8f46d7edfdc
JamesDHW <JamesHaworthWheatman@gmail.com>
2026-01-15 12:32:41 +0000

    chore: tell robots to stop crawling

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      crawlDelay: 30,
     },
     sitemap: `${DOMAIN}/sitemap.xml`,
   };
